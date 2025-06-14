999d59f28ddd69b671a21298ddf3d41cf015d78f
Umut <arazumut349@gmail.com>
2025-06-14 18:28:53 +0300

    Remove loan-related templates: borrowed_books.html, lent_books.html, loan_detail.html, loan_form.html, loan_list.html, overdue_books.html, return_book.html

--- LibraryManagement/libraries/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Library
-
-@admin.register(Library)
-class LibraryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'owner', 'created_at')
-    list_filter = ('created_at',)
-    search_fields = ('name', 'description', 'owner__username')
-    date_hierarchy = 'created_at'
